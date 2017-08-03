6c066dfb57e43bb6b03094310c573cb4feaf1119
bassbone <bassbone0o_o0@yahoo.co.jp>
2017-08-03 12:34:44 +0000

    sort

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 import "math/rand"
 import "sort"
+import "time"
 
 func shuffle(list []int) {
     for i := len(list); i > 1; i-- {
@@ -11,14 +12,25 @@ func shuffle(list []int) {
     }
 }
 
-func main() {
-    list := make([]int, 10, 10)
-    for i := 0; i < 10; i++ {
+func make_list(n int) []int{
+    list := make([]int, n, n)
+    for i := 0; i < n; i++ {
         list[i] = i + 1
     }
     shuffle(list)
+    return list
+}
+
+func main() {
+    list := make_list(100)
     fmt.Println(list)
-    sort.Sort(list)
+
+    start := time.Now()
+    sort.Sort(sort.IntSlice(list))
+    end := time.Now()
+
     fmt.Println(list)
+
+    fmt.Printf("%f秒\n", (end.Sub(start)).Seconds())
 }
 
