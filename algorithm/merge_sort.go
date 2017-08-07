e28429b31bfc5ac1374181d12c048aab6616e42a
bassbone <bassbone0o_o0@yahoo.co.jp>
2017-08-07 23:20:07 +0900

    Merge Sort 途中

--- algorithm/merge_sort.go
+++ algorithm/merge_sort.go
@@ -12,10 +12,19 @@ func MergeSort(list []int) {
             list[0] = tmp
         }
     } else {
-        min_num := int(len(list) / 2)
-        arr1 = MergeSort()
-        arr2 = MergeSort()
-        list = 
+        mid_num := int(len(list) / 2)
+        arr1 := make([]int, mid_num + 1, mid_num + 1)
+        arr2 := make([]int, len(list) - mid_num, len(list) - mid_num)
+        arr1 = list[0:mid_num]
+        arr2 = list[mid_num,len(list) - 1]
+        MergeSort(arr1)
+        MergeSort(arr2)
+        list = merge_arr(arr1, arr2)
     }
 }
 
+func merge_arr(arr1 []int, arr2 []int) []int{
+    list := make([]int, len(arr1) + len(arr2))
+    while (len(arr1) || len(arr2)) {
+        if (len(arr1) == 0) {
+            
